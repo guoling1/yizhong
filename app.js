4b3a2f809ef052b86e9872e36e6832ec4db1c0eb
guoling1 <646626114@qq.com>
2019-04-08 08:23:17 +0800

    1

--- app.js
+++ app.js
@@ -1,215 +1,163 @@
 //app.js
 var mydata = [{
-  name: '北京',
-  selected: false
-}, {
-  name: '天津',
-  selected: false
-},
-{
-  name: '上海',
-  selected: false
-}, {
-  name: '重庆',
-  selected: true
-},
-{
-  name: '河北',
-  selected: true
-}, {
-  name: '河南',
-  selected: false
-},
-{
-  name: '云南',
-  selected: false
-}, {
-  name: '辽宁',
-  selected: false
-},
-{
-  name: '黑龙江',
-  selected: false
-}, {
-  name: '湖南',
-  selected: false
-},
-{
-  name: '安徽',
-  selected: false
-}, {
-  name: '山东',
-  selected: false
-},
-{
-  name: '新疆',
-  selected: false
-}, {
-  name: '江苏',
-  selected: false
-},
-{
-  name: '浙江',
-  selected: false
-}, {
-  name: '江西',
-  selected: false
-},
-{
-  name: '湖北',
-  selected: false
-}, {
-  name: '广西',
-  selected: false
-},
-{
-  name: '甘肃',
-  selected: false
-}, {
-  name: '山西',
-  selected: false
-},
-{
-  name: '内蒙古',
-  selected: false
-}, {
-  name: '陕西',
-  selected: false
-},
-{
-  name: '吉林',
-  selected: false
-}, {
-  name: '福建',
-  selected: false
-},
-{
-  name: '贵州',
-  selected: false
-}, {
-  name: '广东',
-  selected: false
-},
-{
-  name: '青海',
-  selected: false
-}, {
-  name: '西藏',
-  selected: false
-},
-{
-  name: '四川',
-  selected: false
-}, {
-  name: '宁夏',
-  selected: false
-},
-{
-  name: '海南',
-  selected: false
-}, {
-  name: '台湾',
-  selected: false
-},
-{
-  name: '香港',
-  selected: false
-}, {
-  name: '澳门',
-  selected: false
-}
+    name: '北京',
+    selected: false
+  }, {
+    name: '天津',
+    selected: false
+  },
+  {
+    name: '上海',
+    selected: false
+  }, {
+    name: '重庆',
+    selected: true
+  },
+  {
+    name: '河北',
+    selected: true
+  }, {
+    name: '河南',
+    selected: false
+  },
+  {
+    name: '云南',
+    selected: false
+  }, {
+    name: '辽宁',
+    selected: false
+  },
+  {
+    name: '黑龙江',
+    selected: false
+  }, {
+    name: '湖南',
+    selected: false
+  },
+  {
+    name: '安徽',
+    selected: false
+  }, {
+    name: '山东',
+    selected: false
+  },
+  {
+    name: '新疆',
+    selected: false
+  }, {
+    name: '江苏',
+    selected: false
+  },
+  {
+    name: '浙江',
+    selected: false
+  }, {
+    name: '江西',
+    selected: false
+  },
+  {
+    name: '湖北',
+    selected: false
+  }, {
+    name: '广西',
+    selected: false
+  },
+  {
+    name: '甘肃',
+    selected: false
+  }, {
+    name: '山西',
+    selected: false
+  },
+  {
+    name: '内蒙古',
+    selected: false
+  }, {
+    name: '陕西',
+    selected: false
+  },
+  {
+    name: '吉林',
+    selected: false
+  }, {
+    name: '福建',
+    selected: false
+  },
+  {
+    name: '贵州',
+    selected: false
+  }, {
+    name: '广东',
+    selected: false
+  },
+  {
+    name: '青海',
+    selected: false
+  }, {
+    name: '西藏',
+    selected: false
+  },
+  {
+    name: '四川',
+    selected: false
+  }, {
+    name: '宁夏',
+    selected: false
+  },
+  {
+    name: '海南',
+    selected: false
+  }, {
+    name: '台湾',
+    selected: false
+  },
+  {
+    name: '香港',
+    selected: false
+  }, {
+    name: '澳门',
+    selected: false
+  }
 ];
 
 App({
-  onLaunch: function () {
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
-    // this.getData()
+  onLaunch: function() {
+    this.getData()
+  },
+  globalData: {
+    userInfo: null,
+    url: 'http://hdjincheng.6655.la',
+    // url:'http://24323j007d.wicp.vip',
+    // url: 'http://tbk.o87.net',
+    chartsData: []
+  },
+  // 获取地图信息
+  getData() {
     var that = this
     wx.request({
       url: this.globalData.url + '/sys/usersAllProvince',
       method: 'get',
-      // header: {
-      //   "Content-Type": "applciation/json"
-      // },
-      success: function (res) {
-        if (res.data.code == 200) {
-          var data = res.data.data;
-          for (var i in data) {
-            for (var j in mydata) {
-              if (mydata[j].name == data[i].province) {
-                mydata[j].selected = true;
-              }
+      success: function(res) {
+        // if (res.data.code == 200) {
+        var data = res.data.data;
+        for (var i in data) {
+          for (var j in mydata) {
+            if (mydata[j].name == data[i].province) {
+              mydata[j].selected = true;
             }
           }
-          that.globalData.chartsData = mydata
-
-        } else {
-          console.log('2解密失败')
         }
-      },
-      fail: function () {
-        console.log('1系统错误')
-      }
-    })
-  },
-  globalData: {
-    userInfo: null,
-    // url:'http://hdjincheng.6655.la',
-    url:'http://24323j007d.wicp.vip',
-    // url: 'http://tbk.o87.net',
-    chartsData:[]
-    
-  },
-  getData() {
-    var that = this;
-    wx.request({
-      url: getApp().globalData.url + '/sys/usersAllProvince',
-      method: 'get',
-      // header: {
-      //   "Content-Type": "applciation/json"
-      // },
-      success: function (res) {
-        if (res.data.code == 200) {
-          var data = res.data.data;
-          for (var i in data) {
-            for (var j in mydata) {
-              if (mydata[j].name == data[i].province) {
-                mydata[j].selected = true;
-              }
-            }
-          }
-          that.globalData.chartsData = res.userInfo
-
-        } else {
-          console.log('2解密失败')
+        that.globalData.chartsData = mydata
+        console.log(1)
+        if (that.employIdCallback) {
+          console.log(2)
+          that.employIdCallback(mydata);
         }
+        // } else {
+        //   console.log('2解密失败')
+        // }
       },
-      fail: function () {
+      fail: function() {
         console.log('1系统错误')
       }
     })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,138 +4,13 @@ const app = getApp();
 import * as echarts from '../../ec-canvas/echarts';
 import geoJson from './mapData.js';
 
-function randomData() {
-  return Math.round(Math.random() * 500);
-}
-var mydata = [{
-  name: '北京',
-  selected: false
-}, {
-  name: '天津',
-  selected: false
-},
-{
-  name: '上海',
-  selected: false
-}, {
-  name: '重庆',
-  selected: true
-},
-{
-  name: '河北',
-  selected: true
-}, {
-  name: '河南',
-  selected: false
-},
-{
-  name: '云南',
-  selected: false
-}, {
-  name: '辽宁',
-  selected: false
-},
-{
-  name: '黑龙江',
-  selected: false
-}, {
-  name: '湖南',
-  selected: false
-},
-{
-  name: '安徽',
-  selected: false
-}, {
-  name: '山东',
-  selected: false
-},
-{
-  name: '新疆',
-  selected: false
-}, {
-  name: '江苏',
-  selected: false
-},
-{
-  name: '浙江',
-  selected: false
-}, {
-  name: '江西',
-  selected: false
-},
-{
-  name: '湖北',
-  selected: false
-}, {
-  name: '广西',
-  selected: false
-},
-{
-  name: '甘肃',
-  selected: false
-}, {
-  name: '山西',
-  selected: false
-},
-{
-  name: '内蒙古',
-  selected: false
-}, {
-  name: '陕西',
-  selected: false
-},
-{
-  name: '吉林',
-  selected: false
-}, {
-  name: '福建',
-  selected: false
-},
-{
-  name: '贵州',
-  selected: false
-}, {
-  name: '广东',
-  selected: false
-},
-{
-  name: '青海',
-  selected: false
-}, {
-  name: '西藏',
-  selected: false
-},
-{
-  name: '四川',
-  selected: false
-}, {
-  name: '宁夏',
-  selected: false
-},
-{
-  name: '海南',
-  selected: false
-}, {
-  name: '台湾',
-  selected: false
-},
-{
-  name: '香港',
-  selected: false
-}, {
-  name: '澳门',
-  selected: false
-}
-];
-
+// let chart;
 function initChart(canvas, width, height) {
   const chart = echarts.init(canvas, null, {
     width: width,
     height: height
   });
-  canvas.setChart(chart);
-  // console.log(geoJson)
-  echarts.registerMap('china', geoJson);
+  
 
   const option = {
     geo: {
@@ -175,14 +50,9 @@ function initChart(canvas, width, height) {
         }
       },
       itemStyle: {
-
         normal: {
           areaColor: '#01215c',
           borderColor: '#9ffcff',
-
-          // borderColor: '#389BB7',
-          // areaColor: '#000',
-          // borderWidth: 2,
         },
         emphasis: {
           // show: false
@@ -197,50 +67,42 @@ function initChart(canvas, width, height) {
     }],
 
   };
-
+  canvas.setChart(chart);
+  echarts.registerMap('china', geoJson);
   chart.setOption(option);
 
   return chart;
 }
-let chart;
 Page({
   data: {
     top: [],
     userInfo: {},
-    hasUserInfo: false,
+    hasMap: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     ec: {
       onInit: initChart
     }
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../community/community'
-    })
-  },
   onLoad: function() {
-    
-    // ajax请求好数据后，调用获取option函数，传一些数据，
-    // 然后用全局变量echarts元素chartLine 来 setOption即可。
-    // var xData=[1,2,3]
-    // var data_cur = [1, 2, 3]
-    // var data_his = [1, 2, 3]
-    // var option = getOption(xData, data_cur, data_his);
-    // chartLine.setOption(option);
-    // //如果上面初始化时候，已经chartLine已经setOption了，
-    // //那么建议不要重新setOption，官方推荐写法，重新赋数据即可。
-    // chartLine.setOption({
-    //   xAxis: {
-    //     data: [1, 2, 3]
-    //   },
-    //   series: [{
-    //     data: data_cur
-    //   }, {
-    //       data: [1, 2, 3]
-    //   }]
-    // });
-
+    var that = this
+    if (app.globalData.chartsData.length != 0) {
+      this.setData({
+        hasMap: true
+      });
+    } else {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.employIdCallback = employId => {
+        if (employId != '') {
+          this.setData({
+            hasMap: true
+          });
+        }
+      }
+    }
+    if (this.data.hasMap) {
+      console.log(11)
+    }
   },
   /**
    * 生命周期函数--监听页面显示
@@ -250,16 +112,9 @@ Page({
       success: function(res) {
         var code = res.code; //登录凭证
         if (code) {
-          console.log(getApp().globalData)
-
           //2、调用获取用户信息接口
           wx.getUserInfo({
             success: function(res) {
-              console.log({
-                encryptedData: res.encryptedData,
-                iv: res.iv,
-                code: code
-              })
               //3.请求自己的服务器，解密用户信息 获取unionId等加密信息
               wx.request({
                 url: getApp().globalData.url + '/decodeUserInfo',
@@ -308,37 +163,8 @@ Page({
       }
     })
     this.getTop();
-    // this.getData();
   },
-  getData() {
-    var that = this;
-    wx.request({
-      url: getApp().globalData.url + '/sys/usersAllProvince',
-      method: 'get',
-      // header: {
-      //   "Content-Type": "applciation/json"
-      // },
-      success: function (res) {
-        if (res.data.code == 200) {
-          var data = res.data.data;
-          for(var i in data){
-            for(var j in mydata){
-              if (mydata[j].name == data[i].province){
-                mydata[j].selected = true;
-              }
-            }
-          }
-          demo2.series[0].data = mydata;
-
-        } else {
-          console.log('2解密失败')
-        }
-      },
-      fail: function () {
-        console.log('1系统错误')
-      }
-    })
-  },
+  // 获取前三名省市
   getTop() {
     var that = this;
     wx.request({
@@ -363,9 +189,6 @@ Page({
     })
   },
   onReady() {
-    setTimeout(function() {
-      // 获取 chart 实例的方式
-      console.log(echarts.setOption)
-    }, 2000);
+    
   }
 })
